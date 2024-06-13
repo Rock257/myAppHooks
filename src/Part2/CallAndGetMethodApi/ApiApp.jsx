9d7134a09c6070fcc25ac88b2cced371d67f6bd1
Rock25 <rahul.16jime049@jietjodhpur.ac.in>
2024-06-13 23:43:02 +0530

    api call multiple table added

--- src/Part2/CallAndGetMethodApi/ApiApp.jsx
+++ src/Part2/CallAndGetMethodApi/ApiApp.jsx
@@ -1,51 +1,98 @@
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
-import './apidata.css'
+import "./apidata.css";
+
+import { Table } from "react-bootstrap";
 
 export default function ApiApp() {
   const [userData, setUserData] = useState([]);
+  const [post ,setPost] =  useState([])
 
   useEffect(() => {
+
     fetch("https://jsonplaceholder.typicode.com/users").then((result) => {
       result.json(result).then((res) => {
         console.log(res);
         setUserData(res);
-       
       });
     });
+
   }, []);
 
+  useEffect(()=>{
+    
+    fetch("https://jsonplaceholder.typicode.com/posts").then((results) => {
+      results.json(results).then((responce) => {
+        console.log(responce);
+        setPost(responce);
+      });
+    });
+  },[])
+
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/todos")
+  //     .then((responce) => {
+  //       responce.json(responce);
+  //     })
+  //     .then((results) => {
+  //       console.log(results);
+  //       // setUserData(result)
+  //     });
+  // }, []);
 
   return (
     <>
       <h1>API data show here</h1>
       <div className="apidata">
-        
-          <table border='1' >
-            <tbody>
+        <table border="1">
+          <tbody>
+            <tr>
+              <td>Id</td>
+              <td>Name</td>
+              <td>Phone</td>
+              <td>Username</td>
+              <td>Website</td>
+            </tr>
+            {userData.map((item) => {
+              return (
                 <tr>
-                    <td>Id</td>
-                    <td>Name</td>
-                    <td>Phone</td>
-                    <td>Username</td>
-                    <td>Website</td>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.phone}</td>
+                  <td>{item.username}</td>
+                  <td>{item.website}</td>
                 </tr>
-                {
-                    userData.map((item)=>{
-                        return(
-                            <tr>
-                                <td>{item.id}</td>
-                                <td>{item.name}</td>
-                                <td>{item.phone}</td>
-                                <td>{item.username}</td>
-                                <td>{item.website}</td>
-                            </tr>
-                        )
-                    })
-                }
-            </tbody>
-          </table>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <h1>User  Table</h1>
+      <div className="user-table">
+        <Table border='1'  id="userTable">
+          <tbody>
+            <tr>
+              
+              <td>Id</td>
+              <td>Title</td>
+              <td>Body</td>
+              
+            </tr>
+
+            {
+              post.map((item)=>{
+                return(
+                  <tr>
+                    <td>{item.id}</td>
+                    <td>{item.title}</td>
+                    <td>{item.body}</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </Table>
       </div>
     </>
   );
